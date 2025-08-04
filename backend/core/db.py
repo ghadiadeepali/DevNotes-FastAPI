3ab6cd54abcb015be8fbf8296ad16fdca189afeb
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-08-04 19:36:16 +0530

    removed Base.metadata.create_all() since alembic will handle db migrations now

--- backend/core/db.py
+++ backend/core/db.py
@@ -4,9 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from .config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
+
+# Base is the foundation for creating all your ORM models. We inherit this in our models
 Base = declarative_base()
 
-def init_db():
-    from backend.models import user  # Import all models here to ensure table creation
-    Base.metadata.create_all(bind=engine)
-    print("✅ All tables created.")
