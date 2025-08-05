6c94fbffb8adff29f2eeca8a57797bcd94082d0c
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-08-05 21:09:07 +0530

    created db session and get_db function that returns a session object to interact with the database

--- backend/core/db.py
+++ backend/core/db.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from .config import DATABASE_URL
-
 engine = create_engine(DATABASE_URL)
 
 # Base is the foundation for creating all your ORM models. We inherit this in our models
 Base = declarative_base()
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
